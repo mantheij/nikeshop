5f596a0c54804e7fda9784330740c39add9caa9d
jmanthei <vitehd.de@gmail.com>
2024-07-13 20:29:18 +0200

    sidebar

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,32 +1,36 @@
 import './home.css';
-import {Suspense} from "react";
+import React, {Suspense} from "react";
 import { Canvas } from '@react-three/fiber';
 import {Environment, OrbitControls, ContactShadows} from "@react-three/drei";
 import Airforce from '../assets/Airforce.jsx';
+import NikeLogo from '../assets/Nike-Logo.png';
 
 function Home() {
 
     return (
         <div>
+            <img id={"nike"} src={NikeLogo} alt="Nike"/>
             <h1>THE NEW AIR FORCE 2</h1>
             <div id={"af1"}>
                 <Canvas camera={{position: [0, 0, 5], fov: 75}}>
                     <ambientLight intensity={0.5}/>
                     <OrbitControls
-                    enablePan={false}
-                    enableZoom={false}
-                    enableRotate={false}
-                    autoRotate={true}
-                    autoRotateSpeed={1}
-                    minDistance={0.3}
-                    maxDistance={0.3}/>
+                        enablePan={false}
+                        enableZoom={false}
+                        enableRotate={false}
+                        autoRotate={true}
+                        autoRotateSpeed={1}
+                        minDistance={0.3}
+                        maxDistance={0.3}/>
                     <Suspense fallback={null}>
-                        <Airforce />
+                        <Airforce/>
                     </Suspense>
                     <Environment preset={'sunset'}/>
-                    <ContactShadows position={[0, -0.1, 0]} rotation={[Math.PI / 10, 0 ,0]} opacity={1} scale={10} blur={1} far={10} resolution={256} color='#000000'/>
+                    <ContactShadows position={[0, -0.1, 0]} rotation={[Math.PI / 10, 0, 0]} opacity={1} scale={10}
+                                    blur={1} far={10} resolution={256} color='#000000'/>
                 </Canvas>
-                <a id={"info"}>MORE INFORMATION</a>
+                <a id={"info"} href="/shop/men/shoes">MORE INFORMATION</a>
+                <a href="/shop/men">KIDS</a>
             </div>
         </div>
 
--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -6,8 +6,14 @@ function Navigation() {
         <nav className="navigation">
             <ul className="navigation-menu">
                 <li><a href="/">SEARCH</a></li>
-                <li><a href="/shops">SIGN IN</a></li>
-                <li><a href="/about">CART</a></li>
+                <li><a href="/signin">SIGN IN</a></li>
+                <li><a href="/cart">CART</a></li>
+            </ul>
+
+            <ul className={"store-menu"}>
+                <li><a href="/shop/men">MEN</a></li>
+                <li><a href="/shop/men">WOMEN</a></li>
+                <li><a href="/shop/men">KIDS</a></li>
             </ul>
         </nav>
     );
